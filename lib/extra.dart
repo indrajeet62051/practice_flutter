960b16b176d36f2cdc71e515d6b8e4448d9f5cde
Indrajeet Raj <indrajeet62051@gmail.com>
2025-05-04 16:44:57 +0530

    practice

--- lib/extra.dart
+++ lib/extra.dart
@@ -25,7 +25,28 @@ class extra extends StatelessWidget{
         Container(
           width: 500,
           height: 500,
-          child: Center(child: Text("Its extra page for practice ")),
+          child: Center(
+            child: Column(
+              children: [
+                Text("Its extra page for practice "),
+                FloatingActionButton(onPressed: (){
+                  showDialog(context: context, builder: (context){
+                    return  AlertDialog(
+                      title: Text("Alert Dialogbox"),
+                      content: Text("it's a example of alert dialogbox "),
+                      actions: [TextButton(onPressed: (){
+                        Navigator.pop(context)  ;
+                      }, child: Column(children: [
+                        Text("ok"),
+                      ],))],
+                    );
+
+                  });
+
+                },child: Icon(Icons.add_alert),)
+              ],
+            ),
+          ),
         ),
     );
   }
