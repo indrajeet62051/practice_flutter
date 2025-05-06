7365ede56c91544cccee0a1d5b42913529ae4148
Indrajeet Raj <indrajeet62051@gmail.com>
2025-05-07 00:33:33 +0530

    practice

---
+++ lib/API_practice.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp( practice());
+}
+
+class practice extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "practicing Api intigration"
+          ,
+      home: mainScreen(),
+
+    );
+  }
+
+}
+
+
+class mainScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("API integration Practice  "),
+        backgroundColor: Colors.red.shade600,
+      ),
+      body: Center(
+        child: Text("check console for output"),
+      ),
+    );
+  }
+
+}
